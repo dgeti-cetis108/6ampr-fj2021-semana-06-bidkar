17e2ce9411e8d9ad2fa44be9162e512225a0d858
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2021-03-25 11:00:33 -0700

    Sesión 02 - 25/03/2021
    Emuladores y como utilizar tu teléfono android para depurar.
    
    AlertDialog

--- app/src/main/java/com/cetis108/semana06matutino/MainActivity.kt
+++ app/src/main/java/com/cetis108/semana06matutino/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.cetis108.semana06matutino
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+    fun mostrarMensaje(mensaje: String) {
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setMessage(mensaje)
+            .setCancelable(false)
+            .setPositiveButton("OK", DialogInterface.OnClickListener { dialog, id ->
+                dialog.cancel()
+            })
+        val alert = dialogBuilder.create()
+        //alert.setMessage("Atención")
+        alert.show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,11 +30,14 @@ class MainActivity : AppCompatActivity() {
         val btn_saludar = findViewById<Button>(R.id.btnSaludar)
         // agregar funcionalidad al evento click del botón saludar
         btn_saludar.setOnClickListener {
-            //val saludo = "Hola Android desde Kotlin"
-            // TODO: mostrar el valor Fulano cuando la caja de texto esté vacía (if-else)
-            val nombre = findViewById<EditText>(R.id.txtNombre).text.toString()
-            val saludo = "Hola $nombre desde Kotlin!"
-            Toast.makeText(this, saludo, Toast.LENGTH_LONG).show()
+            var nombre = findViewById<EditText>(R.id.txtNombre).text.toString()
+
+            // Kevin Omar Cabanillas Nuñez
+            if (nombre == "") {
+                nombre = "Fulano"
+            }
+            val saludo = "Hola $nombre desde Kotlin"
+            mostrarMensaje(saludo)
         }
     }
 }
